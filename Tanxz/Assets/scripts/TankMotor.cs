c9f03ff649720affca2e696f5c2e9fa33359ca1a
Conrad Horton <conrad.horton@gmail.com>
2018-02-14 19:41:58 -0800

    Starting work...

---
+++ Tanxz/Assets/scripts/TankMotor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/******************************************************************************
+* TankMotor */
+/**
+* Controls the movement and firing of the tank.
+******************************************************************************/
+public class TankMotor : MonoBehaviour
+  {
+  private CharacterController characterController;
+  /**************************************************************************
+  * Unity Methods 
+  **************************************************************************/
+	void Start ()
+    {
+		
+	  }
+	
+	void Update ()
+    {
+		
+	  }
+  }
